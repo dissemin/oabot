2bfe7b5324ef79cc60b81ffc392bd2dd1b7b2bc5
Antonin Delpeuch <antonin@delpeuch.eu>
2018-05-27 19:13:16 +0200

    Add test utilities for proposed changes - feel free to add other cases

---
+++ src/tests/templateedit.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+
+from oabot.main import *
+
+class TemplateEditTests(unittest.TestCase):
+
+    def propose_change(self, text, page_name='Test page'):
+        wikicode = mwparserfromhell.parse(text)
+        for template in wikicode.filter_templates():
+            edit = TemplateEdit(template, page_name)
+            edit.propose_change()
+            return edit
+
+    def test_add_arxiv(self):
+        edit = self.propose_change("""
+{{Cite journal|last=Prpić|first=John|last2=Shukla|first2=Prashant P.|last3=Kietzmann|first3=Jan H.|last4=McCarthy|first4=Ian P.|date=2015-01-01|title=How to work a crowd: Developing crowd capital through
+crowdsourcing|url=http://www.sciencedirect.com/science/article/pii/S0007681314001438|journal=Business Horizons|volume=58|issue=1|pages=77–85|doi=10.1016/j.bushor.2014.09.005}}
+        """)
+        self.assertEquals("arxiv=1702.04214", edit.proposed_change)
+
