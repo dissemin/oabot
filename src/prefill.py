286381466f6d33f6dd291def70c475c7f7a71f2c
Federico Leva <federicoleva@tiscali.it>
2021-01-03 15:52:49 +0200

    Remove starting_page option, not really supported since the list is shuffled

--- src/prefill.py
+++ src/prefill.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 # -*- coding: utf-8  -*-
 """ Script to run the OAbot suggestion creation on all relevant English Wikipedia pages """
 #
@@ -12,39 +12,27 @@ import sys
 import random
 import requests
 from app import get_proposed_edits, app
-import threading
 from time import sleep
 
 def worker(title=None):
+    print(p.title().encode('utf-8'))
     try:
-        get_proposed_edits(title, False, True, True)
+        get_proposed_edits(page_name=p.title(), force=False, follow_redirects=True, only_doi=True)
     except:
-        pass
+        sleep(60)
 
-def prefill_cache(max_pages=5000, starting_page=None):
+def prefill_cache(max_pages=5000):
     site = pywikibot.Site()
     # pages = pywikibot.Page(site, 'Module:Citation/CS1').embeddedin(namespaces=[0])
     pages = pywikibot.Page(site, 'Digital object identifier').backlinks(namespaces=[0])
     count = 0
-    starting_page_seen = starting_page is None
     sortedpages = []
     for p in pages:
         sortedpages.append(p)
     random.shuffle(sortedpages)
     for p in sortedpages:
-        print(p.title().encode('utf-8'))
-        if p.title() == starting_page:
-           starting_page_seen = True
-           continue
         if count >= max_pages:
             break
-        if not starting_page_seen:
-            continue
-        try:
-            get_proposed_edits(p.title(), False, True, True)
-            #threading.Thread(target=worker, args=[p.title()]).start()
-        except:
-            sleep(60)
         count += 1
         sleep(1.5)
 
