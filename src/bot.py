4f11121993b63c775ddc958b1e97fe45018327ee
Federico Leva <federicoleva@tiscali.it>
2023-01-29 15:07:01 +0200

    Catch exception for protected page

--- src/bot.py
+++ src/bot.py
@@ -39,7 +39,7 @@ def run_bot_on_page(proposed_edits, template_param, access_token=None, site=None
             return True
     except ValueError:
         app.logger.exception('perform_bot_edit failed on {}'.format(page_name))
-    except (pywikibot.exceptions.OtherPageSaveError, requests.exceptions.ReadTimeout):
+    except (pywikibot.exceptions.LockedPageError, pywikibot.exceptions.OtherPageSaveError, requests.exceptions.ReadTimeout):
         app.logger.exception('perform_bot_edit was rejected or timed out on {}'.format(page_name))
     return False
 
